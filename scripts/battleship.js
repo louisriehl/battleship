42aae82b6a89b295a5e3bd0a3f981e88dc0a3468
Louis Riehl <louisriehl@gmail.com>
2018-10-08 20:27:33 +0000

    can now generate a grid

--- scripts/battleship.js
+++ scripts/battleship.js
@@ -1,10 +1,19 @@
 $(document).ready(function() {
   $("#demo").html("Hello there!");
   $("#grid-size").click( () => {
-    makeGrid(6);
+    makeGrid(8);
   });
 });
 
-function makeGrid (thing) {
-
+function makeGrid (size) {
+  $('body').append('<table>');
+  let table = $('table').addClass("battle-grid");
+  for (let row = 0; row < size; row++) {
+    let tr = $('<tr>').addClass(`row-${row}`);
+    table.append(tr);
+    for (let col = 0; col < size; col++) {
+      let td = $('<td>').addClass(`row-${row}-col-${col}`);
+      tr.append(td);
+    }
+  }
 }
