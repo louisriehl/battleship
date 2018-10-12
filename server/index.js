172f03214823fe5cf5cecf3fb8bfb0033e2df190
Louis Riehl <louisriehl@gmail.com>
2018-10-12 21:41:34 +0000

    basic express server built

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 
-const PORT          = 8000;
+const PORT          = 8080;
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+app.get("/", (res, req) => {
+});
 
 app.listen(PORT, () => {
   console.log("Battleship listening on port " + PORT);
